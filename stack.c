9d557196987aa312978a6d650224a7d54122d1e5
YounesFetouaki <104641370+YounesFetouaki@users.noreply.github.com>
2023-10-18 12:59:12 +0100

    power on

---
+++ stack.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+ * stack - Sets the stack mode to stack.
+ * @head: Pointer to the stack's head
+ * @counter: Line number
+ * Return: No return value
+ */
+void stack(stack_t **head, unsigned int counter)
+{
+    (void)head;
+    (void)counter;
+    bus.lifi = 0;
+}
